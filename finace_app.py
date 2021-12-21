eb0d468e1b8f2450af55ed032d5e7a89f35f5689
Hindy <69097028+HindyDS@users.noreply.github.com>
2021-12-21 21:05:13 +0800

    Update finace_app.py

--- finace_app.py
+++ finace_app.py
@@ -252,7 +252,7 @@ elif display_format == "Fractions Of Capital":
 if 'personal_portfolio' not in st.session_state:
     st.session_state['personal_portfolio'] = []  
 
-add_ticker = st.sidebar.text_input(label="Add To Portfolio", value="Type a stock symbol", key="add_ticker")    
+# add_ticker = st.sidebar.text_input(label="Add To Portfolio", value="Type a stock symbol", key="add_ticker")    
     
-option = st.sidebar.selectbox('Watch List', st.session_state['personal_portfolio'])
-st.sidebar.write(option)
+# option = st.sidebar.selectbox('Watch List', st.session_state['personal_portfolio'])
+# st.sidebar.write(option)
