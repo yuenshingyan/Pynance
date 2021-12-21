ae39cc264330c14834d393b031598521147f3caf
Hindy <69097028+HindyDS@users.noreply.github.com>
2021-12-21 20:15:20 +0800

    Update finace_app.py

--- finace_app.py
+++ finace_app.py
@@ -245,7 +245,7 @@ if display_format == "Percentages":
     
     cols_name3[0].dataframe(port_max_sharpe_pct)
     cols_name3[1].dataframe(performance_stats)
-    add_ticker = cols_name3[2].text_input(label="Please type in a stock symbol.", value="AAPL", key="add_ticker")
+    add_ticker = cols_name3[2].text_input(label="Please type in a stock symbol.", value="SYMBOL", key="add_ticker")
     
 elif display_format == "Fractions Of Capital":
     performance_stats.iloc[0, :] = performance_stats.iloc[0, :] * capital
