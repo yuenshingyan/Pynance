6158f947ef24eade59c7e60d7c8aed22da482934
Hindy <69097028+HindyDS@users.noreply.github.com>
2021-12-22 18:56:58 +0800

    Update finace_app.py

--- finace_app.py
+++ finace_app.py
@@ -223,9 +223,8 @@ start_date_port_opt = cols_name2[1].date_input("From", one_year_ago, key="port_o
 end_date_port_opt = cols_name2[2].date_input("To", today, key="port_opt")
 capital = cols_name2[3].number_input('Capital', value=10000)
 
-for t in tickers.split(","):
-  if ticker.isupper() and len(ticker) <= 5:
-    acp = get_adj_close_prices(tickers.split(","), start_date, end_date)
+
+acp = get_adj_close_prices(tickers.split(","), start_date, end_date)
   
 cleaned_weights_min_volatility, cleaned_weights_max_sharpe, performance_stats_min_volatility, performance_stats_max_sharpe = port_opt(acp)
 
