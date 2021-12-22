ba07aa4fea32653d5bc3106efde422557211a909
Hindy <69097028+HindyDS@users.noreply.github.com>
2021-12-22 23:15:04 +0800

    Update finace_app.py

--- finace_app.py
+++ finace_app.py
@@ -282,12 +282,11 @@ value_at_risk = var(acp.pct_change(-1).dropna(), capital, choose_condidence_lvl)
 
 investing_period = end_date_port_opt - start_date_port_opt
 cols_name4[0].subheader("Value At Risk") 
+choose_condidence_lvl = st.slider("Confidence Level", .05)
 cols_name4[0].write(f"{choose_condidence_lvl * 100}% confidence that our portfolio of ${capital} will not exceed losses greater than ${round(value_at_risk, 2)} over a {investing_period.days} day period.")
 
 # Conditional Value At Risk
 cols_name4[1].subheader("Conditional Value At Risk") 
-
-choose_condidence_lvl = st.slider("Confidence Level", .05)
 
 # Side Bar
 add_ticker = st.sidebar.text_input(label="Add To Watchlist", value="Type a stock symbol", key="add_ticker")    
