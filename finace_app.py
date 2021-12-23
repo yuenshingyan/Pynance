59da0175e4a9b8c8db89f3ff267c36d9267ae6e6
Hindy <69097028+HindyDS@users.noreply.github.com>
2021-12-23 16:37:21 +0800

    Update finace_app.py

--- finace_app.py
+++ finace_app.py
@@ -116,7 +116,7 @@ def regime_detection(historical_price, ticker):
   TOOLS = "pan, wheel_zoom, box_zoom, reset, save"
 
   # Historical Price
-  p_historical = figure(x_axis_type="datetime", tools=TOOLS, width=1250, height=400)
+  p_historical = figure(x_axis_type="datetime", tools=TOOLS, width=1300, height=400)
   p_historical.xaxis.major_label_orientation = pi/4
   p_historical.grid.grid_line_alpha=0.3
 
@@ -125,7 +125,7 @@ def regime_detection(historical_price, ticker):
   p_historical.vbar(historical_price.index[dec], w, historical_price["Open"][dec], historical_price["Adj Close"][dec], fill_color="#F2583E", line_color="black", legend_label="Adjusted Close Price (Dec)")
 
   # Log Return with High Volatility
-  p_log_ret = figure(x_axis_type="datetime", x_range=p_historical.x_range, width=1250, height=200)
+  p_log_ret = figure(x_axis_type="datetime", x_range=p_historical.x_range, width=1300, height=200)
   p_log_ret.xaxis.major_label_orientation = pi/4
   p_log_ret.grid.grid_line_alpha=0.3
   
