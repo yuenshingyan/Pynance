317bed058141390d60523eeee58de216cba46dd7
Hindy <69097028+HindyDS@users.noreply.github.com>
2021-12-25 22:43:38 +0800

    Update finace_app.py

--- finace_app.py
+++ finace_app.py
@@ -471,7 +471,7 @@ st.title('Pynance')
 st.header("Regime Detection")
 cols_regime_detection = st.columns(3)
 cols_regime_detection2 = st.columns(8)
-cols_regime_detection3 = st.columns([.75, .5, 2, .5, 3])
+cols_regime_detection3 = st.columns([.75, .5, .75, .5, 3])
 one_year_ago = today - datetime.timedelta(365)
 
 ticker = cols_regime_detection[0].text_input(label="Please type in a stock symbol.", value="AAPL")
